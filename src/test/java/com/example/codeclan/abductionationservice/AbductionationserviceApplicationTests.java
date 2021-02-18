970c061afd220e9fe9fcb55688eab04f26452f2f
Robyn Tyler <robynstyler@outlook.com>
2021-02-18 21:08:08 +0000

    first commit

---
+++ src/test/java/com/example/codeclan/abductionationservice/AbductionationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.abductionationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbductionationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
